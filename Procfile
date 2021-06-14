3cbcf94ffed77321cc14ee729a93beb07cedeff4
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-06-14 11:35:38 +0530

    add Procfile optionally loading .env

---
+++ Procfile
@@ -0,0 +1,1 @@
+webL npm run start
--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-exports.GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID || ''
-exports.GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET || ''
-exports.HOST=process.env.HOST || 'localhost'
-exports.PORT=process.env.PORT || 8080
-exports.SECRET=process.env.SECRET || 'some_secret'
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || ''
+exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
+exports.HOST = process.env.HOST || 'localhost'
+exports.PORT = process.env.PORT || 8080
+exports.SECRET = process.env.SECRET || 'some_secret'
